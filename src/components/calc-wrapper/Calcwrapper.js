db47a4c4c09846c4c7161be8d1909393ba421e1b
Pass80 <71611559+Pass80@users.noreply.github.com>
2021-08-29 16:36:44 +0200

    creating the project components

---
+++ src/components/calc-wrapper/Calcwrapper.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../button/Button'
+import Input from '../input/Input';
+import './Calcwrapper.css';
+
+ function Calcwrapper() {
+	return (
+		<div className="calc-wrapper">
+			<Input />
+			<Button />
+			
+		</div>
+	)
+}
+
+export default Calcwrapper;
