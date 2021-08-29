12e03499e9b9d13492333e53941ff49b33a34dbe
Pass80 <71611559+Pass80@users.noreply.github.com>
2021-08-30 00:15:05 +0200

    adding some components

--- src/components/calc-wrapper/Calcwrapper.js
+++ src/components/calc-wrapper/Calcwrapper.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '../button/Button'
 import Input from '../input/Input';
 import './Calcwrapper.css';
 
+ 
+
+
+
  function Calcwrapper() {
+
+	const [display, setDisplayValue] = useState(""
+   );
+
 	return (
 		<div className="calc-wrapper">
-			<Input />
-			<Button />
+			<Input onScreen={display} />
+			<Button addValue={setDisplayValue} />
 			
 		</div>
 	)
--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Input.css';
 
- function Input() {
+ function Input(props) {
+	 console.log(props)
 	return (
-		<div className="input">
-			
-		</div>
-	)
+    <div className="display">
+      <p>{props.onScreen}</p>
+      <p>{JSON.stringify({ a: 1, b: 3, c: "hi" })}</p>
+    </div>
+  );
 }
 
 export default Input;
